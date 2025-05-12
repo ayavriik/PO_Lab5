b2b0994433f1f618a678ac27776c97c7574d542e
DESKTOP-LOU0FN9 <yarusha4true@gmail.com>
2025-05-12 03:36:37 +0300

    cmt_3

--- server.cpp
+++ server.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 #include <winsock2.h>
+#include <thread>
 #include <string>
 #include <fstream>
 
 #pragma comment(lib, "ws2_32.lib")
 #define PORT 3030
 #define BUFFER_SIZE 1024
+
+void handleClient(SOCKET client) {
+    // прийом і парсинг запиту
+    char buffer[BUFFER_SIZE];
+    int received = recv(client, buffer, BUFFER_SIZE, 0);
+    std::string request(buffer, received);
+
+    auto firstLineEnd = request.find('\n');
+    std::string firstLine = request.substr(0, firstLineEnd);
+    size_t start = firstLine.find(" ") + 2;
+    size_t end = firstLine.rfind(" ");
+    std::string path = firstLine.substr(start, end - start);
+    if (path == "/") path = "index.html";
+
+    // віддаємо файл або 404
+    std::ifstream file(path, std::ios::binary);
+    std::string body;
+    std::string status;
+    if (file) {
+        body.assign((std::istreambuf_iterator<char>(file)),
+                    std::istreambuf_iterator<char>());
+        status = "HTTP/1.1 200 OK\r\n";
+    } else {
+        body = "404 Not Found";
+        status = "HTTP/1.1 404 Not Found\r\n";
+    }
+
+    std::string response = status +
+        "Content-Type: text/html\r\n" +
+        "Content-Length: " + std::to_string(body.size()) + "\r\n\r\n" +
+        body;
+
+    send(client, response.c_str(), response.size(), 0);
+    closesocket(client);
+}
 
 int main() {
     WSADATA wsaData;
@@ -22,37 +58,8 @@ int main() {
         SOCKET client = accept(serverSocket, nullptr, nullptr);
         if (client == INVALID_SOCKET) break;
 
-        char buffer[BUFFER_SIZE];
-        int received = recv(client, buffer, BUFFER_SIZE, 0);
-        std::string request(buffer, received);
-
-        auto firstLineEnd = request.find('\n');
-        std::string firstLine = request.substr(0, firstLineEnd);
-        size_t start = firstLine.find(" ") + 2;
-        size_t end = firstLine.rfind(" ");
-        std::string path = firstLine.substr(start, end - start);
-        if (path == "/") path = "index.html";
-
-        std::ifstream file(path, std::ios::binary);
-        std::string body;
-        std::string status;
-
-        if (file) {
-            body.assign((std::istreambuf_iterator<char>(file)),
-                        std::istreambuf_iterator<char>());
-            status = "HTTP/1.1 200 OK\r\n";
-        } else {
-            body = "404 Not Found";
-            status = "HTTP/1.1 404 Not Found\r\n";
-        }
-
-        std::string response = status +
-            "Content-Type: text/html\r\n" +
-            "Content-Length: " + std::to_string(body.size()) + "\r\n\r\n" +
-            body;
-
-        send(client, response.c_str(), response.size(), 0);
-        closesocket(client);
+        // запускаємо новий потік і «відпускаємо» його назад у систему
+        std::thread(handleClient, client).detach();
     }
 
     closesocket(serverSocket);
